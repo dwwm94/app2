d9e0968ccc873c8352c1d570b9abfa5a03fcab7c
Adimi Nico <dwwm94@gmail.com>
2021-04-13 11:54:30 +0200

    add nav

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -8,14 +8,47 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
-    /**
+   /**
      * 
      *@Route("/car", name="car")
      */
     public function index(): Response
     {
         return $this->render('car/index.html.twig', [
-            'controller_name' => 'CarController',
+             'controller_name' => 'CarController',
+        ]);
+    }
+
+    /**
+     * 
+     *@Route("/", name="app_home")
+     */
+    public function accueil(): Response
+    {
+        return $this->render('car/accueil.html.twig', [
+           
+        ]);
+    }
+
+    /**
+     * 
+     *@Route("/about", name="app_about")
+     */
+    public function about(): Response
+    {
+        return $this->render('car/about.html.twig', [
+            
+        ]);
+    }
+
+    /**
+     * 
+     *@Route("/contact", name="app_contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('car/contact.html.twig', [
+            
         ]);
     }
 }
